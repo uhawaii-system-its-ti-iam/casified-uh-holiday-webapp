146f85665d9f0aef412b47713d807a5f9f627ebe
Jordan Wong <42422209+JorWo@users.noreply.github.com>
2023-10-19 16:09:59 -1000

    Move Iron Secret out of code tree (#25)

--- app/next.config.js
+++ app/next.config.js
@@ -1,4 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
+
+const dotenv = require('dotenv');
+const os = require('os')
+
+dotenv.config({path: `${os.homedir()}/.${os.userInfo().username}-conf/casified-uh-holiday-webapp-overrides.properties`})
+
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
