5f8fb233dbd7b741e6c30c6257ea9ecf8096e4bf
Prayag Das <prayag.j.das2001@gmail.com>
2024-02-05 18:15:34 -1000

    added error.tsx which will render any runtime errors as text on the webpage.
    Added Axios get request for data in page.tsx
    
    Moved No Data Available message to error.tsx
    
    removed unused imports from index page.tsx

--- app/app/(index)/page.tsx
+++ app/app/(index)/page.tsx
@@ -1,22 +1,16 @@
-'use client';
-
 import type { NextPage } from 'next';
-import { Container, Col, Row, Spinner } from 'react-bootstrap';
+import { Container, Col, Row } from 'react-bootstrap';
 import HolidaysTable from "@/components/tables/holidays_table/HolidaysTable";
-import useAxiosPromise from '@/hooks/useAxiosPromise';
+import axios from '../../hooks/axiosConfig';
 
-const Home: NextPage = () => {
-    const { data, isLoading } = useAxiosPromise('holidays');
+const Home: NextPage = async () => {
+    const { data } = await axios.get(process.env.NEXT_PUBLIC_BASE_URL + 'holidays');
 
     return (
         <Container fluid>
             <Row>
                 <Col xs={{ span: 10, offset: 1 }}>
-                    {isLoading ? (
-                        <Spinner animation="border" />
-                    ) : (
-                        <HolidaysTable data={data ? data.data : null} />
-                    )}
+                    <HolidaysTable data={data ? data.data : null} />
                 </Col>
             </Row>
         </Container>
--- app/components/tables/holidays_table/HolidaysTable.tsx
+++ app/components/tables/holidays_table/HolidaysTable.tsx
@@ -42,61 +42,53 @@ const HolidaysTable = ({ data }: HolidaysTableProps) => {
 
     return (
         <div className="container mx-auto p-4">
-            {data === null ? (
-                <div className="text-center">
-                    <p className="font-bold text-2xl">No Data Available</p>
-                </div>
-            ) : (
-                <>
-                    <div className="flex justify-end mb-4">
-                        <Filter filtering={filtering} setFiltering={setFiltering} />
-                    </div>
-                    <table className="groupings-table">
-                        <thead>
-                            {tableInstance.getHeaderGroups().map((headerGroup) => (
-                                <tr key={headerGroup.id}>
-                                    {headerGroup.headers.map((header) => (
-                                        <th
-                                            key={header.id}
-                                            onClick={header.column.getToggleSortingHandler()}
-                                            scope="col"
-                                            className="px-2 py-2.5"
-                                            style={{ minWidth: '400px' }}
-                                        >
-                                            <div className="header-content">
-                                                {header.isPlaceholder ? null :
-                                                    flexRender(header.column.columnDef.header, header.getContext())}
-                                                <SortArrow sortDirection={header.column.getIsSorted()} />
-                                            </div>
-                                        </th>
-                                    ))}
-                                </tr>
-                            ))}
-                        </thead>
-                        <tr className={`border-b border-off-white`}></tr>
-                        <tbody>
-                            {tableInstance.getRowModel().rows.map((row, index: number) => (
-                                <tr
-                                    className={`${
-                                        index % 2 === 0
-                                            ? 'bg-top'
-                                            : 'bg-transparent'
-                                    } border-b border-off-white`}
-                                    key={row.id}
+            <div className="flex justify-end mb-4">
+                <Filter filtering={filtering} setFiltering={setFiltering} />
+            </div>
+            <table className="groupings-table">
+                <thead>
+                    {tableInstance.getHeaderGroups().map((headerGroup) => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => (
+                                <th
+                                    key={header.id}
+                                    onClick={header.column.getToggleSortingHandler()}
+                                    scope="col"
+                                    className="px-2 py-2.5"
+                                    style={{ minWidth: '400px' }}
                                 >
-                                    {row.getVisibleCells().map((cell) => (
-                                        <td key={cell.id} className="p-2">
-                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                        </td>
-                                    ))}
-                                </tr>
+                                    <div className="header-content">
+                                        {header.isPlaceholder ? null :
+                                            flexRender(header.column.columnDef.header, header.getContext())}
+                                        <SortArrow sortDirection={header.column.getIsSorted()} />
+                                    </div>
+                                </th>
                             ))}
-                        </tbody>
+                        </tr>
+                    ))}
+                </thead>
+                <tr className={`border-b border-off-white`}></tr>
+                <tbody>
+                    {tableInstance.getRowModel().rows.map((row, index: number) => (
+                        <tr
+                            className={`${
+                                index % 2 === 0
+                                    ? 'bg-top'
+                                    : 'bg-transparent'
+                            } border-b border-off-white`}
+                            key={row.id}
+                        >
+                            {row.getVisibleCells().map((cell) => (
+                                <td key={cell.id} className="p-2">
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
 
-                    </table>
-                    <Pagination tableInstance={tableInstance} />
-                </>
-            )}
+            </table>
+            <Pagination tableInstance={tableInstance} />
         </div>
     );
 }
